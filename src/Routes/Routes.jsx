39536f2fe238441207bfceddc53922c0436afb24
Samuel <saaamuelrooodrigooo@gmail.com>
2025-01-23 15:27:58 -0300

    addd

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Links, Route, Routes } from "react-router-dom";
 import Homepage from "../Pages/Homepage/index-homepage";
-import Boleto from "../Pages/Boletos/index-boletos";
-{/*import Modelos from "../Pages/Modelos/index-modelos";*/}
+{/*import Boleto from "../Pages/Boletos/index-boletos";*/}
+import Modelos from "../Pages/Modelos/index-modelos";
 import Ferramentas from "../Pages/Ferramentas/index-ferramentas";
 import AppLinks from "../Pages/Links/links";
 import Preços from "../Pages/Preços/index-precos";

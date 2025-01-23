e4fac5e2ff25a4c5225bad43bd5e2b55ef4e3b19
Samuel <saaamuelrooodrigooo@gmail.com>
2025-01-23 14:41:52 -0300

    Add Resto

--- src/Pages/Agenda/index-agenda.jsx
+++ src/Pages/Agenda/index-agenda.jsx
@@ -1,14 +1,195 @@
 import Footer from "../../Components/Footer/footer";
 import Header from "../../Components/Header/header";
 import { DivBox, ContainerDiv, Backgrounder, ImgBackground } from "./styles-agenda";
-import BackgroundPapelURL from '../../assets/Background-papelamassado.jpg'
+import { ContainerTitulo, ContainerCalendario, ContainerCalendarioTitulo, ContainerCalendarioDiv, ContainerCalendarioDias, ContainerCalendarioLine1, ContainerCalendarioLine1Letter, ContainerCalendarioLine2, ContainerCalendarioLine2Circle, ContainerCalendarioLine2CircleDiv } from './styles-agenda';
+import { ContainerCalendarioMarcacao, ContainerCalendarioMarcacaoDiv, ContainerCalendarioMarcacaoElement, ContainerCalendarioMarcacaoElementCor, ContainerCalendarioMarcacaoElementTexto } from './styles-agenda'
+import { ContainerTarefas, ContainerTarefasInputs, ContainerInputsDiv, ContainerTarefasBox, ContainerTarefasBoxRed } from './styles-agenda'
+import BackgroundPapelURL from '../../assets/Background-papelamassado.jpg';
+import boxicons from "boxicons";
 
 function Agenda(){
     return(
         <DivBox>
             <Header />
             <ContainerDiv>
-                <h1>Agenda</h1>
+                <ContainerTitulo>
+                    <h1>Agenda</h1>
+                </ContainerTitulo>
+                <ContainerCalendario>
+                    <ContainerCalendarioDiv>
+                        <ContainerCalendarioDias>
+                            <ContainerCalendarioTitulo>
+                                <h2>Janeiro de 2025</h2>
+                            </ContainerCalendarioTitulo>
+                            <ContainerCalendarioLine1>
+                                <ContainerCalendarioLine1Letter>D</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>S</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>T</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>Q</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>Q</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>S</ContainerCalendarioLine1Letter>
+                                <ContainerCalendarioLine1Letter>S</ContainerCalendarioLine1Letter>
+                            </ContainerCalendarioLine1>
+                            <ContainerCalendarioLine2>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>1</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>2</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>3</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>4</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>5</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>6</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>7</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>8</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>9</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>10</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>11</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>12</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>13</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>14</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>15</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>16</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>17</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>18</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>19</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>20</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>21</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>22</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>23</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>24</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>25</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>26</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>27</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>28</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>29</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>30</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2CircleDiv>
+                                <ContainerCalendarioLine2Circle>31</ContainerCalendarioLine2Circle>
+                                </ContainerCalendarioLine2CircleDiv>
+                            </ContainerCalendarioLine2>
+                        </ContainerCalendarioDias>
+                        <ContainerCalendarioMarcacao>
+                            <hr></hr>
+                            <ContainerCalendarioMarcacaoDiv>
+                                <ContainerCalendarioMarcacaoElement>
+                                    <ContainerCalendarioMarcacaoElementCor></ContainerCalendarioMarcacaoElementCor>
+                                    <ContainerCalendarioMarcacaoElementTexto>xxxxxxx</ContainerCalendarioMarcacaoElementTexto>
+                                </ContainerCalendarioMarcacaoElement>
+                                <ContainerCalendarioMarcacaoElement>
+                                    <ContainerCalendarioMarcacaoElementCor></ContainerCalendarioMarcacaoElementCor>
+                                    <ContainerCalendarioMarcacaoElementTexto>xxxxxxxx</ContainerCalendarioMarcacaoElementTexto>
+                                </ContainerCalendarioMarcacaoElement>
+                                <ContainerCalendarioMarcacaoElement>
+                                    <ContainerCalendarioMarcacaoElementCor></ContainerCalendarioMarcacaoElementCor>
+                                    <ContainerCalendarioMarcacaoElementTexto>xxxxxxx</ContainerCalendarioMarcacaoElementTexto>
+                                </ContainerCalendarioMarcacaoElement>
+                                <ContainerCalendarioMarcacaoElement>
+                                    <ContainerCalendarioMarcacaoElementCor></ContainerCalendarioMarcacaoElementCor>
+                                    <ContainerCalendarioMarcacaoElementTexto>xxxxx</ContainerCalendarioMarcacaoElementTexto>
+                                </ContainerCalendarioMarcacaoElement>
+                            </ContainerCalendarioMarcacaoDiv>
+                        </ContainerCalendarioMarcacao>
+                    </ContainerCalendarioDiv>
+                </ContainerCalendario>
+                <ContainerTarefas>
+                    <ContainerTarefasInputs>
+                        <ContainerInputsDiv>
+                            <button id="novo">Novo Trabalho <box-icon name='plus'></box-icon></button>
+                        </ContainerInputsDiv>
+                        <ContainerInputsDiv>
+                            <button id="alterar">Alterar Trabalho</button>
+                        </ContainerInputsDiv>
+                        <ContainerInputsDiv>
+                            <button id="excluir">Excluir Trabalho <box-icon name='minus'></box-icon></button>
+                        </ContainerInputsDiv>
+                        <ContainerInputsDiv>
+                            <button id="finalizar">Finalizar Trabalho <box-icon name='check'></box-icon></button>
+                        </ContainerInputsDiv>
+                    </ContainerTarefasInputs>
+                    <ContainerTarefasBox>
+                        <ContainerTarefasBoxRed></ContainerTarefasBoxRed>
+                        <ContainerTarefasBoxRed></ContainerTarefasBoxRed>
+                        <ContainerTarefasBoxRed></ContainerTarefasBoxRed>
+                        <ContainerTarefasBoxRed></ContainerTarefasBoxRed>
+                    </ContainerTarefasBox>
+                </ContainerTarefas>
+                {/* 
+                <ContainerNovaTarefa>
+                    <ContainerNovaTarefaBox>
+                        <ContainerNovaTarefaTitulo>
+                            <h2>Tarefa Novo</h2>
+                        </ContainerNovaTarefaTitulo>
+                        <Form action="">
+                           <label htmlFor="">Complexidade: </label>
+                           <ContainerNovaTarefaDivs>
+                                <select>
+                                    <option value=""></option>
+                                    <option value="vermelho">Vermelho</option>
+                                </select>
+                           </ContainerNovaTarefaDivs>
+
+                        </Form>
+                    </ContainerNovaTarefaBox>
+                </ContainerNovaTarefa>
+                */}
             </ContainerDiv>
                 <Backgrounder>
                     <ImgBackground src={BackgroundPapelURL} alt="" />
--- src/Pages/Agenda/styles-agenda.js
+++ src/Pages/Agenda/styles-agenda.js
@@ -6,7 +6,7 @@ export const DivBox = styled.div `
     flex-direction: column;
     background-color: #ffffff;
     width: 1280px;
-    height: 2700px;
+    height: 3000px;
     justify-content: space-between;
     @media (${breakpoints.mg}){
         height: 2500px;
@@ -21,7 +21,6 @@ export const DivBox = styled.div `
         height: 2000px;
     }
 `
-
 //Background
 export const Backgrounder = styled.div `
     display: flex;
@@ -58,7 +57,7 @@ export const ContainerDiv = styled.div `
     width: 1280px;
     height: 2700px;
     position: absolute;
-    text-align: center;
+    align-items: center;
     font-size: 30px;
     z-index: 1;
     @media (${breakpoints.mg}){
@@ -76,7 +75,151 @@ export const ContainerDiv = styled.div `
     @media (${breakpoints.sm}){
         height: 300px;
     }
-    h1{
-        padding: 150px 0px 0px 0px;
+`
+export const ContainerTitulo = styled.div`
+    display: flex;
+    padding: 165px 0px 50px 0px;
+`   
+export const ContainerCalendario = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    height: 580px;
+    padding: 0px 0px 80px 0px;
+`
+export const ContainerCalendarioTitulo = styled.div`
+    display: flex;
+    width: 100%;
+    height: 50px;
+    padding: 0px 0px 25px 0px;
+    justify-content: center;
+`
+export const ContainerCalendarioDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 520px;
+`
+export const ContainerCalendarioDias = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+`
+export const ContainerCalendarioLine1 = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 90%;
+    justify-content: space-between;
+`   
+export const ContainerCalendarioLine1Letter = styled.div`
+    display: flex;
+    width: 60px;
+    height: 60px;
+    font-size: 35px;
+    font-weight: bolder;
+    padding: 0px 0px 0px 25px;
+`
+export const ContainerCalendarioLine2 = styled.div`
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+`
+export const ContainerCalendarioLine2Circle = styled.div`
+    display: flex;
+    font-size: 40px;
+    background-color: #B9B9B9;
+    width: 80px;
+    height: 80px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    font-weight: bolder;
+`
+export const ContainerCalendarioLine2CircleDiv = styled.div`
+    display: flex;
+    padding: 10px 20px 0px 0px;
+`
+export const ContainerCalendarioMarcacao = styled.div`
+    display: flex;
+    padding: 80px 0px 0px 0px;
+    height: 500px;
+`
+export const ContainerCalendarioMarcacaoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 500px;
+`
+export const ContainerCalendarioMarcacaoElement = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 0px 0px 0px 20px;
+`
+export const ContainerCalendarioMarcacaoElementCor = styled.div`
+    display: flex;
+    width: 50px;
+    height: 50px;
+    background-color: red;
+    border-radius: 100px;
+`
+export const ContainerCalendarioMarcacaoElementTexto = styled.div`
+    display: flex;
+    font-size: 30px;
+    justify-content: center;
+    padding: 0px 0px 0px 10px;
+
+`
+export const ContainerTarefas = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 600px;
+    width: 95%;
+`
+export const ContainerTarefasInputs = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    padding: 0px 0px 20px 0px;
+    button{
+        height: 70px;
+        font-size: 30px;
+        font-weight: bolder;
+        border-color: #B9B9B9;
+        background-color: #ffffff;
+        border-radius: 10px;
+        border: 1px;
+        border-style: solid;
+        padding: 0px 10px 0px 10px;
     }
+    button:hover{
+        cursor: pointer;
+        transition: ease-in-out;
+        transform: scale(1.13);
+        background-color:rgb(240, 240, 240);
+    }
+`
+export const ContainerInputsDiv = styled.div`
+    display: flex;
+`
+export const ContainerTarefasBox = styled.div`
+    display: flex;
+    width: 100%;
+    min-height: 700px;
+    flex-direction: column;
+    background-color: #ffffff;
+    border: 1px;
+    border-color: #B9B9B9;
+    border-style: solid;
+    border-radius: 10px;
+    justify-content: space-evenly;
+    align-items: center;
+`
+export const ContainerTarefasBoxRed = styled.div`
+    display: flex;
+    width: 90%;
+    height: 125px;
+    border: 2px;
+    border-top: 5px;
+    border-style: solid;
+    border-color: #FF0000;
 `

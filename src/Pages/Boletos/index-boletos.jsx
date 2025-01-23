1eff223242241a501023eade405ffb97b28bf794
Samuel <saaamuelrooodrigooo@gmail.com>
2025-01-23 15:12:52 -0300

    ajuste

--- src/Pages/Boletos/index-boletos.jsx
+++ src/Pages/Boletos/index-boletos.jsx
@@ -1,7 +1,7 @@
 import Footer from "../../Components/Footer/footer";
 import Header from "../../Components/Header/header";
 import { DivBox, ContainerDiv, Backgrounder, ImgBackground } from "./styles-Boletos";
-import { ContainerSubDiv, ContainerBox, ContainerBoxSubDiv, ContainerBoxTitle, ContainerBoxImg, ContainerBoxTextoTitle, ContainerBoxTexto, } from "./styles-Boletos";
+import { ContainerSubDiv, ContainerBox, ContainerBoxSubDiv, ContainerBoxTitle, ContainerBoxImg, ContainerBoxTextoTitle, ContainerBoxTexto } from "./styles-Boletos";
 import BackgroundPapelURL from '../../assets/Background-papelamassado.jpg'
 import EquatorialURL from '../../assets/Equatorial.png'
 

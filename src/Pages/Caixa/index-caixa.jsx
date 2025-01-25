96eda99b6bc38a0e21a0d1484d0de7735fe5655b
Samuel <saaamuelrooodrigooo@gmail.com>
2025-01-25 03:40:00 -0300

    Atualização

--- src/Pages/Caixa/index-caixa.jsx
+++ src/Pages/Caixa/index-caixa.jsx
@@ -1,14 +1,180 @@
 import Footer from "../../Components/Footer/footer";
 import Header from "../../Components/Header/header";
 import { DivBox, ContainerDiv, Backgrounder, ImgBackground } from "./styles-caixa";
+import { ContainerTitulo, ContainerButtons, ContainerArrow } from './styles-caixa'
 import BackgroundPapelURL from '../../assets/Background-papelamassado.jpg'
+import VendasURL from '../../assets/Caixa/Vendas.png'
+import EstoqueURL from '../../assets/Caixa/Estoque.png'
+import FechamentoURL from '../../assets/Caixa/Fechamento.png'
+import boxicons from "boxicons";
+import { ContainerCaixa, ContainerSubDivCaixa, ContainerCaixaBox, ContainerCaixaBoxTitulo, ContainerThirdDivCaixa, ContainerCaixaButton, ContainerFourDivCaixa, ContainerCaixaBoxProdutoDescricao} from './styles-caixa';
+import { ContainerProdutoDescricaoTitulo, ContainerProdutoDescricaoItem, ContainerProdutoDescricaoCodigo, ContainerProdutoDescricaoDescricao, ContainerProdutoDescricaoQtd, ContainerProdutoDescricaoUnid, ContainerProdutoDescricaoTotal } from './styles-caixa';
+import { ContainerProdutoDescricaoTexto, ContainerProdutoDescricaoTextoItem, ContainerProdutoDescricaoTextoCodigo, ContainerProdutoDescricaoTextoDescricao, ContainerProdutoDescricaoTextoQtd, ContainerProdutoDescricaoTextoUnid, ContainerProdutoDescricaoTextoTotal, ContainerFiveDivCaixa } from './styles-caixa'
+
 
 function Caixa(){
     return(
         <DivBox>
             <Header />
             <ContainerDiv>
-                <h1>Caixas</h1>
+                <ContainerTitulo>
+                    <h1>Caixas</h1>
+                </ContainerTitulo>
+                <ContainerButtons>
+                        <img src={VendasURL} alt="" />
+                        <img src={EstoqueURL} alt="" />
+                        <img src={FechamentoURL} alt="" />
+                </ContainerButtons>
+                <ContainerArrow>
+                    <box-icon name='chevron-up' size='lg'></box-icon>
+                </ContainerArrow>
+                <ContainerCaixa>
+                    <ContainerSubDivCaixa>
+                        {/*Código de Barras*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Código de Barras</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <p>000000000000000000000000</p>
+                        </ContainerCaixaBox>
+                        {/*Subtotal*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Subtotal</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <p>R$00,00</p>
+                        </ContainerCaixaBox>
+                        {/*Metodo de Pagamento*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Metodo de Pagamento</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <select name='pagamentos'>
+                                    <option value=""></option>
+                                    <option value="A vista"></option>
+                                    <option value="Crédito"></option>
+                                    <option value="Débito"></option>
+                                    <option value="Pix"></option>
+                                </select>
+                        </ContainerCaixaBox>
+                        {/*Total Recebido*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Total Recebido</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <p>R$00,00</p>
+                        </ContainerCaixaBox>
+                        {/*Troco*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Troco</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <p>R$00,00</p>
+                        </ContainerCaixaBox>
+                        {/*Btn Finalizar e Cancelar*/}
+                        <ContainerThirdDivCaixa>
+                            <ContainerCaixaButton>
+                                <h2>Finalizar Venda</h2>
+                            </ContainerCaixaButton>
+                            <ContainerCaixaButton>
+                                <h2>Cancelar Venda</h2>
+                            </ContainerCaixaButton>
+                        </ContainerThirdDivCaixa>
+                    </ContainerSubDivCaixa>
+                    <ContainerSubDivCaixa>
+                        <ContainerFourDivCaixa>
+                            {/*Valor Unitário*/}
+                            <ContainerCaixaBox>
+                                <ContainerCaixaBoxTitulo>
+                                    <h2>Valor Unitário</h2>
+                                </ContainerCaixaBoxTitulo>
+                                    <p>R$00,00</p>
+                            </ContainerCaixaBox>
+                            {/*Quantidade*/}
+                            <ContainerCaixaBox>
+                                <ContainerCaixaBoxTitulo>
+                                    <h2>Quant.</h2>
+                                </ContainerCaixaBoxTitulo>
+                                    <p>01</p>
+                            </ContainerCaixaBox>
+                            {/*Total de Produtos*/}
+                            <ContainerCaixaBox>
+                                <ContainerCaixaBoxTitulo>
+                                    <h2>Total do Produto</h2>
+                                </ContainerCaixaBoxTitulo>
+                                    <p>R$00,00</p>
+                            </ContainerCaixaBox>
+                        </ContainerFourDivCaixa>
+                        {/*Lista de Produtos*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Lista de Produtos</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <ContainerCaixaBoxProdutoDescricao>
+                                    <ContainerProdutoDescricaoTitulo>
+                                        <ContainerProdutoDescricaoItem>
+                                            <p>N° Item</p>
+                                        </ContainerProdutoDescricaoItem>
+                                        <ContainerProdutoDescricaoCodigo>
+                                            <p>Codigo</p>
+                                        </ContainerProdutoDescricaoCodigo>
+                                        <ContainerProdutoDescricaoDescricao>
+                                            <p>Descrição</p>
+                                        </ContainerProdutoDescricaoDescricao>
+                                        <ContainerProdutoDescricaoQtd>
+                                            <p>Qtd.</p>
+                                        </ContainerProdutoDescricaoQtd>
+                                        <ContainerProdutoDescricaoUnid>
+                                            <p>Valor Unid.</p>
+                                        </ContainerProdutoDescricaoUnid>
+                                        <ContainerProdutoDescricaoTotal>
+                                            <p>Valor Total</p>
+                                        </ContainerProdutoDescricaoTotal>
+                                    </ContainerProdutoDescricaoTitulo>
+                                    <ContainerProdutoDescricaoTexto>
+                                        <ContainerProdutoDescricaoTextoItem>
+                                            <p>0</p>
+                                        </ContainerProdutoDescricaoTextoItem>
+                                        <ContainerProdutoDescricaoTextoCodigo>
+                                            <p>0000000000</p>
+                                        </ContainerProdutoDescricaoTextoCodigo>
+                                        <ContainerProdutoDescricaoTextoDescricao>
+                                            <p>xxxxxxxxxxxxxxxxxxx</p>
+                                        </ContainerProdutoDescricaoTextoDescricao>
+                                        <ContainerProdutoDescricaoTextoQtd>
+                                            <p>0,00</p>
+                                        </ContainerProdutoDescricaoTextoQtd>
+                                        <ContainerProdutoDescricaoTextoUnid>
+                                            <p>0,00</p>
+                                        </ContainerProdutoDescricaoTextoUnid>
+                                        <ContainerProdutoDescricaoTextoTotal>
+                                            <p>0,00</p>
+                                        </ContainerProdutoDescricaoTextoTotal>
+                                    </ContainerProdutoDescricaoTexto>
+                                </ContainerCaixaBoxProdutoDescricao>
+                        </ContainerCaixaBox>
+                        {/*Comandos*/}
+                        <ContainerCaixaBox>
+                            <ContainerCaixaBoxTitulo>
+                                <h2>Comandos</h2>
+                            </ContainerCaixaBoxTitulo>
+                                <p>F1 - XXXXXX</p>
+                                <p>F1 - XXXXXX</p>
+                                <p>F1 - XXXXXX</p>
+                                <p>F1 - XXXXXX</p>
+                                <p>F1 - XXXXXX</p>
+                        </ContainerCaixaBox>
+                        {/*Btn Imprimir e Salvar*/}
+                        <ContainerFiveDivCaixa>
+                            <ContainerCaixaButton>
+                                <h2>Imprimir Nota</h2>
+                            </ContainerCaixaButton>
+                            <ContainerCaixaButton>
+                                <h2>Salvar em PDF</h2>
+                            </ContainerCaixaButton>
+                        </ContainerFiveDivCaixa>
+                    </ContainerSubDivCaixa>
+                </ContainerCaixa>
             </ContainerDiv>
                 <Backgrounder>
                     <ImgBackground src={BackgroundPapelURL} alt="" />
--- src/Pages/Caixa/styles-caixa.js
+++ src/Pages/Caixa/styles-caixa.js
@@ -58,7 +58,7 @@ export const ContainerDiv = styled.div `
     width: 1280px;
     height: 2700px;
     position: absolute;
-    text-align: center;
+    align-items: center;
     font-size: 30px;
     z-index: 1;
     @media (${breakpoints.mg}){
@@ -76,7 +76,148 @@ export const ContainerDiv = styled.div `
     @media (${breakpoints.sm}){
         height: 300px;
     }
-    h1{
-        padding: 150px 0px 0px 0px;
+`
+export const ContainerTitulo = styled.div`
+    display: flex;
+    padding: 165px 0px 35px 0px;
+`
+export const ContainerButtons = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    padding: 0px 0px 20px;
+    img{
+        border: 1px;
+        border-radius: 10px;
+        border-style: solid;
+        border-color: #B9B9B9;
     }
+    img:hover{
+        cursor: pointer;
+        transition: ease 0.8s;
+        transform: scale(1.2);
+    }
+`
+export const ContainerArrow = styled.div`
+    display: flex;
+    box-icon:hover{
+        cursor: pointer;
+        border-radius: 100px;
+        background-color: #B9B9B9;
+        transition: ease-in 0.5s;
+        transform: scale(1.2);
+    }
+`
+//#1
+export const ContainerCaixa = styled.div`
+    display: flex;
+`
+export const ContainerSubDivCaixa = styled.div`
+    display: flex;
+`
+export const ContainerCaixaBox = styled.div`
+    display: flex;
+`
+export const ContainerCaixaBoxTitulo = styled.div`
+    display: flex;
+`
+export const ContainerThirdDivCaixa = styled.div`
+    display: flex;
+`
+export const ContainerCaixaBoxProdutoDescricao = styled.div`
+    display: flex;
+`
+export const ContainerCaixaBoxProdutoDescricaoTitulo = styled.div`
+    display: flex;
+`
+//#2
+export const ContainerProdutoDescricaoTitulo = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoItem = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoCodigo = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoDescricao = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoQtd = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoUnid = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTotal = styled.div`
+    display: flex;
+`
+//#3
+export const ContainerProdutoDescricaoTexto = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoItem = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoCodigo = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoDescricao = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoQtd = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoUnid = styled.div`
+    display: flex;
+`
+export const ContainerProdutoDescricaoTextoTotal = styled.div`
+    display: flex;
+`
+export const ContainerFiveDivCaixa = styled.div`
+    display: flex;
+`
+//#4
+export const ContainerEstoque = styled.div`
+    display: flex;
+`
+export const ContainerEstoqueButtonDiv = styled.div`
+    display: flex;
+`
+export const ContainerEstoqueButton = styled.div`
+    display: flex;
+`
+export const ContainerSubDivEstoque = styled.div`
+    display: flex;
+`
+export const ContainerEstoqueBox = styled.div`
+    display: flex;
+`
+export const ContainerEstoqueBoxTitulo = styled.div`
+    display: flex;
+`
+//#5
+export const ContainerFechamento = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoTitulo = styled.div`
+    display: flex;
+`
+export const ContainerSubDivFechamento = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoVendedor = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoData = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoDescricao = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoButtonDiv = styled.div`
+    display: flex;
+`
+export const ContainerFechamentoButton = styled.div`
+    display: flex;
 `
